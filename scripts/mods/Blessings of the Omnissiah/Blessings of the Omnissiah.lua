b4706d68521f325eb4d3f450e7edda757c5d2952
Ben <Lai>
2023-03-03 18:24:23 +1100

    added in lower ranking filter

--- scripts/mods/Blessings of the Omnissiah/Blessings of the Omnissiah.lua
+++ scripts/mods/Blessings of the Omnissiah/Blessings of the Omnissiah.lua
@@ -1,3 +1,10 @@
+--[[
+Title: Blessings of the Omnissiah
+Author: Wobin
+Date: 02/02/2023
+Repository: https://github.com/Wobin/BlessingsOfTheOmnissiah
+]]--
+
 require("scripts/ui/view_content_blueprints/item_blueprints")
 local mod = get_mod("Blessings of the Omnissiah")
 local ItemUtils = require("scripts/utilities/items")
@@ -13,34 +20,69 @@ local indexBlessings = function(template)
     end)
 end
 
-local BlessingVisibilitySlot = function(item, index)      
+local isLowerRank = function(trait) 
+  local rank = trait.rarity + 1
+  while mod.blessings[trait.id] and mod.blessings[trait.id][rank] ~= nil do
+    if mod.blessings[trait.id][rank] == "seen" then      
+      return true;
+    end    
+    rank = rank + 1
+  end
+  return false;
+end
+
+local traitVisibility = function(trait,lower_rank_indicator)  
+  if mod:get("lowerRankFilter") == 1 then
+    if lower_rank_indicator then 
+      return false
+    else
+      return mod.blessings[trait.id] and mod.blessings[trait.id][trait.rarity] == "unseen"   
+    end
+  elseif mod:get("lowerRankFilter") == 2 then
+    if not isLowerRank(trait) then
+      return not lower_rank_indicator and mod.blessings[trait.id] and mod.blessings[trait.id][trait.rarity] == "unseen"        
+    else
+      return lower_rank_indicator and mod.blessings[trait.id] and mod.blessings[trait.id][trait.rarity] == "unseen"   
+    end
+  elseif mod:get("lowerRankFilter") == 3 then
+    if isLowerRank(trait) then
+      return false      
+    end
+    if not lower_rank_indicator then 
+      return mod.blessings[trait.id] and mod.blessings[trait.id][trait.rarity] == "unseen"   
+    end
+  end  
+end
+
+local BlessingVisibilitySlot = function(item, index, lower_rank_indicator)      
   if item.traits == nil or item.traits[index] == nil then     
     return false
   end   
+  local trait = item.traits[index]   
   local template = ItemUtils.trait_category(item)
   if template and not mod.traitCategory[template] then
     indexBlessings(template)    
     return false      
   end
-  local trait = item.traits[index]   
-  return mod.blessings[trait.id] and mod.blessings[trait.id][trait.rarity] == "unseen"   
+  return traitVisibility(trait, lower_rank_indicator)
 end
 
-local BlessingVisibilityItem = function(content, index)    
+local BlessingVisibilityItem = function(content, index, lower_rank_indicator)    
   if content.element.item.traits == nil or content.element.item.traits[index] == nil then     
     return false
   end 
   local trait = content.element.item.traits[index]   
+  
   local template = string.match(trait.id, "content/items/traits/([^/]+)/%S+")
   if template and not mod.traitCategory[template] then
     indexBlessings(template)        
     return false
-  end    
-  return mod.blessings[trait.id] and mod.blessings[trait.id][trait.rarity] == "unseen"   
+  end  
+  return traitVisibility(trait, lower_rank_indicator)
 end
 
 mod.item_blessings= {
-  {
+            {
             pass_type = "texture",
             value = "content/ui/materials/symbols/new_item_indicator",
             style_id = "blessings_icon_b1",
@@ -58,7 +100,25 @@ mod.item_blessings= {
                   return false
                 end
             },
-              {
+            {
+            pass_type = "texture",
+            value = "content/ui/materials/symbols/new_item_indicator",
+            style_id = "blessings_icon_b1a",
+            style = {
+               vertical_alignment = "bottom",
+                horizontal_alignment = "left",
+                color = Color.dark_orange(255, true),
+                offset = {28, 0, 10},
+                size = {35, 35},                
+            },
+            visibility_function = function (content)                                                                  
+                if content.trait_1 then                                     
+                   return BlessingVisibilityItem(content, 1, true)
+                  end
+                  return false
+                end
+            },
+            {
             pass_type = "texture",
             value = "content/ui/materials/symbols/new_item_indicator",
             style_id = "blessings_icon_b2",
@@ -72,6 +132,24 @@ mod.item_blessings= {
             visibility_function = function (content)        
                    if content.trait_2 then                   
                     return BlessingVisibilityItem(content, 2)
+                  end
+                  return false
+                end
+            },
+             {
+            pass_type = "texture",
+            value = "content/ui/materials/symbols/new_item_indicator",
+            style_id = "blessings_icon_b2a",
+            style = {
+               vertical_alignment = "bottom",
+                horizontal_alignment = "left",
+                color = Color.dark_orange(255, true),
+                offset = {70, 0, 10},
+                size = {35, 35},                
+            },
+            visibility_function = function (content)        
+                   if content.trait_2 then                   
+                    return BlessingVisibilityItem(content, 2, true)
                   end
                   return false
                 end
@@ -90,6 +168,24 @@ mod.item_blessings= {
             visibility_function = function (content)        
                    if content.trait_3 then 
                     return BlessingVisibilityItem(content, 3)
+                  end
+                  return false
+                end
+            },
+             {
+            pass_type = "texture",
+            value = "content/ui/materials/symbols/new_item_indicator",
+            style_id = "blessings_icon_b3a",
+            style = {
+               vertical_alignment = "bottom",
+                horizontal_alignment = "left",
+                color = Color.dark_orange(255, true),
+                offset = {112, 0, 10},
+                size = {35, 35},                
+            },
+            visibility_function = function (content)        
+                   if content.trait_3 then 
+                    return BlessingVisibilityItem(content, 3, true)
                   end
                   return false
                 end
@@ -114,7 +210,25 @@ mod.itemslot_blessings = {
                   end
                   return false
                 end
-            }, {
+            },
+             {
+            pass_type = "texture",
+            value = "content/ui/materials/symbols/new_item_indicator",
+            style_id = "blessings_icon_a1c",
+            style = {
+               vertical_alignment = "bottom",
+                horizontal_alignment = "left",
+                color = Color.dark_orange(255, true),
+                offset = {28, 0, 10},
+                size = {35, 35},                
+            },
+            visibility_function = function (content)        
+                  if content.item ~= nil and content.item.traits ~= nil then                    
+                    return BlessingVisibilitySlot(content.item, 1, true)
+                  end
+                  return false
+                end
+            },{
             pass_type = "texture",
             value = "content/ui/materials/symbols/new_item_indicator",
             style_id = "blessings_icon_a2",
@@ -128,6 +242,23 @@ mod.itemslot_blessings = {
             visibility_function = function (content)        
                   if content.item ~= nil and content.item.traits ~= nil  then                    
                     return BlessingVisibilitySlot(content.item, 2)
+                  end
+                  return false
+                end
+          },{
+            pass_type = "texture",
+            value = "content/ui/materials/symbols/new_item_indicator",
+            style_id = "blessings_icon_a2",
+            style = {
+               vertical_alignment = "bottom",
+                horizontal_alignment = "left",
+                color = Color.dark_orange(255, true),
+                offset = {70, 0, 10},
+                size = {35, 35},                
+            },
+            visibility_function = function (content)        
+                  if content.item ~= nil and content.item.traits ~= nil  then                    
+                    return BlessingVisibilitySlot(content.item, 2, true)
                   end
                   return false
                 end
@@ -145,6 +276,23 @@ mod.itemslot_blessings = {
             visibility_function = function (content)        
                   if content.item ~= nil and content.item.traits ~= nil  then                    
                     return BlessingVisibilitySlot(content.item, 3)
+                  end
+                  return false
+                end
+          }, {
+            pass_type = "texture",
+            value = "content/ui/materials/symbols/new_item_indicator",
+            style_id = "blessings_icon_a3",
+            style = {
+               vertical_alignment = "bottom",
+                horizontal_alignment = "left",
+                color = Color.dark_orange(255, true),
+                offset = {112, 0, 10},
+                size = {35, 35},                
+            },
+            visibility_function = function (content)        
+                  if content.item ~= nil and content.item.traits ~= nil  then                    
+                    return BlessingVisibilitySlot(content.item, 3, true)
                   end
                   return false
                 end
@@ -177,7 +325,6 @@ mod.on_all_mods_loaded = function()
     return data
   end)
   mod:hook_safe(CLASS.CraftingExtractTraitView,"_perform_crafting", function(self)
-      print("Purging cache")
       mod.blessings = {}
       mod.traitCategory = {}
       end)
--- scripts/mods/Blessings of the Omnissiah/Blessings of the Omnissiah_localization.lua
+++ scripts/mods/Blessings of the Omnissiah/Blessings of the Omnissiah_localization.lua
@@ -4,5 +4,17 @@ return {
   },
   mod_description = {
 		en = "Places an indicator on blessings that have yet to be offered to the Omnissiah",
-	}  
+	},  
+  lowerRankFilter = {
+    en = "Filter out lower ranked Blessings"
+    },
+  no_filter_localization_id = {
+    en = "No filter applied"
+  },
+  show_lower_rank_localization_id = {
+    en = "Show lower ranks of gained blessings with an amber indicator"
+  },
+  hide_lower_rank_localization_id = {
+    en = "Hide lower ranks of gained blessings"
+    },
 }
